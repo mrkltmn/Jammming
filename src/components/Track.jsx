3a042f0a8cdff52f3e648e3cbe375984a86e292c
mrkltmn <mark.altman2@gmail.com>
2026-06-23 21:17:11 -0500

    Create track component.

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 function Track({ track }) {
     return (
-
+        <div>
+            <img
+                src={track.art}
+                alt={`album art for ${track.album} by ${track.artist} from Apple iTunes`} />
+            <h3>{track.title}</h3>
+            <h4>{track.artist}</h4>
+            <h4>{track.album}</h4>
+        </div>
     )
 };
 
